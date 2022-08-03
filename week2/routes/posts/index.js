555cbdabf00578f2dbb80058ec30e494e9e6a221
Mmd Nemati <mmd.nematiii@gmail.com>
2022-08-03 23:29:40 +0430

    Implement get /users and /users/:id

--- week2/routes/posts/index.js
+++ week2/routes/posts/index.js
@@ -6,4 +6,15 @@ posts.use(express.json());
 
 let postsData = [];
 
+posts.get('/posts', (req, res) => {
+    res.send(postsData);
+});
+
+posts.get('/posts/:id', (req, res) => {
+    const post = postsData.find(p => p.id === req.params.id);
+    if(!post) return res.status(404).send(`Post with ID ${req.params.id} not found.`);
+
+    res.send(post);
+})
+
 export { posts, postsData };
