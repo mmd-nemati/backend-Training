57cd23a30e6cf8694eff2a17b7c0ff0062abd8d3
Mmd Nemati <mmd.nematiii@gmail.com>
2022-08-04 20:05:06 +0430

    Implement get /likes and /likes/:id

--- week2/routes/likes/index.js
+++ week2/routes/likes/index.js
@@ -6,4 +6,21 @@ import Joi from 'joi';
 const likes = express();
 likes.use(express.json());
 
+let likesData = [];
+
+likes.get('/likes', (req, res) => {
+    res.send(likesData);
+});
+
+likes.get('/likes/:id', (req, res) => {
+    const like = findLikeById(req.params.id);
+    if(!like) return res.status(404).send(`Like with ID ${req.params.id} not found.`);
+
+    res.send(like);
+});
+
+function findLikeById(id) {
+    return likesData.find(l => l.id === parseInt(id));
+}
+
 export { likes };
