c65dca51e87c3e2e1605f59a43cb5ec03f63ba5a
Mmd Nemati <mmd.nematiii@gmail.com>
2022-08-12 15:19:51 +0430

    Conncet to mongodb

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 import express from 'express';
-import { routes } from './routes/index.js';
+import mongoose from 'mongoose';
+import config from 'config';
+import { routes } from './routes/routes.js';
 const port = process.env.PORT || 5000;
+
+mongoose.connect(config.get('dbpath'))
+    .then(() => console.log(`Connected to MongoDB...`))
+    .catch(() => console.error(`Error connecting to MongoDB...`));
 
 const app = express();
 app.use('/', routes);
