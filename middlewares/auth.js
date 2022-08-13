c915e9c274f5ae527462b5d159bc6b7c55cc3aca
Mmd Nemati <mmd.nematiii@gmail.com>
2022-08-13 21:13:05 +0430

    Implement authorization middlewate

---
+++ middlewares/auth.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+import config from 'config';
+
+function auth(req, res, next) {
+    try {
+        const token = req.header(config.get('authTokenName'));
+        if (!token) return res.status(401).send('Access denied. No token provided.');
+        
+        const payload = jwt.decode(token, config.get('jwtPrivateKey'));
+        req.user = payload;
+        next();
+    }
+    catch (err) {
+        res.status(400).send('Invalid token.');
+    }
+}
+
+export { auth };
