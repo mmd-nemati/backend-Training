0219a3fc5df60601a4151c1b09010b703b558535
Mmd Nemati <mmd.nematiii@gmail.com>
2022-08-25 16:18:30 +0430

    Modify token expiration result

--- middlewares/authn.js
+++ middlewares/authn.js
@@ -7,19 +7,20 @@ async function authn(req, res, next) {
         if (!token) return res.status(401).send('Access denied. No token provided.');
 
         const payload = jwt.decode(token, config.get('jwtPrivateKey'), { complete: true });
-       
+
         let dateNow = new Date();
         if (!payload.exp) throw new Error();
-        if (payload.exp < dateNow.getTime()/1000) throw new Error('TokenExpiredError');
+        if (payload.exp < dateNow.getTime() / 1000) throw new Error('TokenExpiredError');
         await jwt.verify(token, config.get('jwtPrivateKey'));
-        
+
         req.user = payload;
         next();
     }
     catch (err) {
-        if (err.name === 'TokenExpiredError') return res.status(401).send('Token expierd.');
+        if (err.message === 'TokenExpiredError')
+            return res.status(401).send('Token expierd.');
         
-        res.status(400).send('Invalid token.');
+            res.status(400).send('Invalid token.');
     }
 }
 
