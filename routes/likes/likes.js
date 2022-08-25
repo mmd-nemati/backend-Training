38d36fc5b817b5dbc6915a678f7261d615a1b846
Mmd Nemati <mmd.nematiii@gmail.com>
2022-08-25 18:14:44 +0430

    Clean some code - Delete PUT route for likes completaly

--- routes/likes/likes.js
+++ routes/likes/likes.js
@@ -1,9 +1,5 @@
 import express from 'express';
 import lodash from 'lodash';
-import { User } from '../../models/user/user.js';
-import { Post } from '../../models/post/post.js';
-import { Like } from '../../models/like/like.js';
-import { setSortOptins, paginate } from '../helper.js';
 import { authn } from '../../middlewares/authn.js'
 import { likeAuthz } from '../../middlewares/likeAuthz.js';
 
@@ -55,25 +51,10 @@ likes.post('/', authn, async (req, res) => {
     }
 });
 
-// As of now, I don't see any reason for that the Like entity 
-// should have a PUT route. So this is commented out until 
-// I figure out what the heck to do with it.
-/*
-likes.put('/:id', (req, res) => {
-    const like = findLikeById(req.params.id);
-    if (!like) return res.status(404).send(`Like with ID ${req.params.id} not found.`);
-    const { error } = validateLikeDepricated(req.body, "put");
-    if (error) return res.status(400).send(error.message);
-
-    ({ userId: like.userId = like.userId, postId: like.postId = like.postId } = req.body);
-    res.send(like);
-});
-*/
-
 likes.delete('/:id', [authn, likeAuthz], async (req, res) => {
     try {
         await unlikePost(req);
-        
+
         res.status(200).send(`Post unliked successfully.`);
     }
     catch (err) {
