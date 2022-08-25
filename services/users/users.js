9536dd52652d57938bdaa75b58c683a54e04fa2e
Mmd Nemati <mmd.nematiii@gmail.com>
2022-08-25 04:41:19 +0430

    Complete user services

--- services/users/users.js
+++ services/users/users.js
@@ -1,4 +1,5 @@
 import lodash from 'lodash';
+import bcrypt from 'bcryptjs';
 import { User } from '../../models/user/user.js';
 import { setSortOptins, paginate } from '../helper.js';
 async function getAllUsers(req) {
@@ -48,4 +49,45 @@ async function signUp(req) {
     }
 }
 
-export { getAllUsers, getOneUser, signUp };
+async function login(req) {
+    try {
+        const user = await User.findOne({ $or: [{ email: req.body.email }, { username: req.body.username }] });
+        if (!user) throw { 'message': 'Invalid credentials.' };
+
+        const valid = await bcrypt.compare(req.body.password, user.password);
+        if (!valid) throw { 'message': 'Invalid credentials.' };
+
+        const token = user.generateAuthToken();
+
+        return { 'token': token };
+    }
+    catch (err) {
+        throw err;
+    }
+}
+
+async function editUser(req) {
+    try { 
+        const user = await User.findOneAndUpdate({ _id: req.params.id }, lodash.pick(req.body, ['name', 'username', 'age', '_id']),
+        { new: true, runValidators: true });
+
+        return { 'user': user };
+    }
+    catch (err) {
+        throw err;
+    }
+}
+
+async function deleteUser(req) {
+    try {
+        const user = await User.findByIdAndRemove(req.params.id, { new: true });
+
+        return { 'user': user };
+
+    } 
+    catch (err) {
+        throw err;
+    }
+}
+
+export { getAllUsers, getOneUser, signUp, login, editUser, deleteUser };
