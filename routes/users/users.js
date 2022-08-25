55ff809ff3e5b6c14208f9673a3283b052392504
Mmd Nemati <mmd.nematiii@gmail.com>
2022-08-25 04:42:13 +0430

    Refactor users route to use services

--- routes/users/users.js
+++ routes/users/users.js
@@ -1,28 +1,22 @@
 import express from 'express';
 import config from 'config';
 import lodash from 'lodash';
-import bcrypt from 'bcryptjs';
-import { User } from '../../models/user/user.js';
-import { setSortOptins, paginate } from '../helper.js';
 import { authn } from '../../middlewares/authn.js';
 import { userAutzh } from '../../middlewares/userAuthz.js';
+
+import {
+    getAllUsers, getOneUser, signUp,
+    login, editUser, deleteUser
+} from '../../services/users/users.js';
 
 const users = express();
 users.use(express.json());
 
 users.get('/', async (req, res) => {
     try {
-        let sortParam = setSortOptins(req.query);
-        const pageOptions = paginate(req.query);
-        
-        const users = await User
-            .find()
-            .select('name username age created_at -_id')
-            .sort(sortParam)
-            .skip((pageOptions.page - 1) * pageOptions.limit)
-            .limit(pageOptions.limit);
+        const result = await getAllUsers(req);
 
-        res.send(users);
+        res.send(result.users);
     }
     catch (err) {
         res.status(500).send(err.message)
@@ -31,26 +25,22 @@ users.get('/', async (req, res) => {
 
 users.get('/:id', async (req, res) => {
     try {
-        const user = await User
-            .findById(req.params.id)
-            .select('name username age created_at -_id');
-        if (!user) return res.status(404).send(`User with ID ${req.params.id} Not Found.`);
+        const result = await getOneUser(req.params.id);
 
-        res.send(user);
+        res.send(result.user);
     }
     catch (err) {
+        if (err.message === 'User not found') return res.status(404).send(`User with ID ${req.params.id} Not Found.`);
+
         res.status(500).send(err.message);
     }
 });
 
 users.post('/', async (req, res) => {
     try {
-        let user = new User(lodash.pick(req.body, ['name', 'username', 'email', 'password', 'age', 'phoneNumber']));
-        user = await user.save();
+        const result = await signUp(req);
 
-        let token = user.generateAuthToken();
-
-        res.header(config.get('authTokenName'), token).status(201).send(lodash.pick(user, ['name', 'username', 'created_at']));
+        res.header(config.get('authTokenName'), result.token).status(201).send(lodash.pick(result.user, ['name', 'username', 'created_at']));
     }
     catch (err) {
         if (err.name === "ValidationError")
@@ -66,14 +56,9 @@ users.post('/', async (req, res) => {
 
 users.post('/login', async (req, res) => {
     try {
-        const user = await User.findOne({ $or: [{ email: req.body.email }, { username: req.body.username }] })
-        if (!user) return res.status(400).send(`Invalid credentials.`);
-        
-        const valid = await bcrypt.compare(req.body.password, user.password);
-        if (!valid) return res.status(400).send(`Invalid credentials.`);
+        const result = await login(req);
 
-        let token = user.generateAuthToken();
-        res.header(config.get('authTokenName'), token).status(200).send(`Logged in successfully.`);
+        res.header(config.get('authTokenName'), result.token).status(200).send(`Logged in successfully.`);
     }
     catch (err) {
         res.status(500).send(err.message);
@@ -82,10 +67,9 @@ users.post('/login', async (req, res) => {
 
 users.put('/:id', [authn, userAutzh], async (req, res) => {
     try {
-        const user = await User.findOneAndUpdate({ _id: req.params.id }, lodash.pick(req.body, ['name', 'username', 'age', '_id']),
-            { new: true, runValidators: true });
+        const result = await editUser(req);
 
-        res.status(200).send(lodash.pick(user, ['name', 'username', 'age']));
+        res.status(200).send(lodash.pick(result.user, ['name', 'username', 'age']));
     }
     catch (err) {
         if (err.name === "ValidationError")
@@ -93,19 +77,18 @@ users.put('/:id', [authn, userAutzh], async (req, res) => {
         if (err.code === 11000 && err.keyPattern.username === 1)
             return res.status(400).send(`Username '${err.keyValue.username}' already exists.`);
 
-        res.status(500).send(err);
+        res.status(500).send(err.message);
     }
 });
 
 users.delete('/:id', [authn, userAutzh], async (req, res) => {
     try {
-        const user = await User.findByIdAndRemove(req.params.id, { new: true });
-        if (!user) return res.status(404).send(`User with ID ${req.params.id} Not Found.`);
+        const result = await deleteUser(req);
 
-        res.status(200).send(`${user.username} was deleted successfully.`);
+        res.status(200).send(`${result.user.username} was deleted successfully.`);
     }
     catch (err) {
-        res.status(500).send(err);
+        res.status(500).send(err.message);
     }
 });
 
